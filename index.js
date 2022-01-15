d736c63d87ad29ee604fbe99463fc3604bd75a2b
Leissan Khan <leissan.khan@gmail.com>
2022-01-15 16:36:02 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+number=10;
+function add() {
+    return(number +=5);
+}
+function divide() {
+    return(number/=1);
+}
+function subtract() {
+    return(number-3);
+}
+function multiply() {
+    return(number*3);
+}
+function add(a,b) {
+    return(a+b);
+}
+
+
+
